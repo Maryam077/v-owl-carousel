6cbf0df1286461c8e14e1c06ad29628f798d9347
Maryam Raafat <maryamraafat077@gmail.com>
2021-03-15 19:04:26 +0200

    Add new properties

--- src/plugin.js
+++ src/plugin.js
@@ -2,6 +2,6 @@ import VOwlCarousel from './Carousel.vue';
 
 module.exports = {
   install: function (Vue, options) {
-    Vue.component('v-owl-carousel', VOwlCarousel);
+    Vue.component('owl-carousel-vuejs', VOwlCarousel);
   }
 };
